31100521aca2ecd9fa180c5fe270aa342e43df43
James Aprosail <aprosail@outlook.com>
2024-05-14 14:38:54 +0800

    wrap column and row

---
+++ lib/src/list.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/widgets.dart';
+
+extension WrapList on List<Widget> {
+  Widget column({
+    Key? key,
+    MainAxisAlignment mainAxisAlignment = MainAxisAlignment.start,
+    MainAxisSize mainAxisSize = MainAxisSize.max,
+    CrossAxisAlignment crossAxisAlignment = CrossAxisAlignment.center,
+    TextDirection? textDirection,
+    VerticalDirection verticalDirection = VerticalDirection.down,
+    TextBaseline? textBaseline,
+  }) =>
+      Column(
+        key: key,
+        mainAxisAlignment: mainAxisAlignment,
+        mainAxisSize: mainAxisSize,
+        crossAxisAlignment: crossAxisAlignment,
+        textDirection: textDirection,
+        verticalDirection: verticalDirection,
+        textBaseline: textBaseline,
+        children: this,
+      );
+
+  Widget row({
+    Key? key,
+    MainAxisAlignment mainAxisAlignment = MainAxisAlignment.start,
+    MainAxisSize mainAxisSize = MainAxisSize.max,
+    CrossAxisAlignment crossAxisAlignment = CrossAxisAlignment.center,
+    TextDirection? textDirection,
+    VerticalDirection verticalDirection = VerticalDirection.down,
+    TextBaseline? textBaseline,
+  }) =>
+      Row(
+        key: key,
+        mainAxisAlignment: mainAxisAlignment,
+        mainAxisSize: mainAxisSize,
+        crossAxisAlignment: crossAxisAlignment,
+        textDirection: textDirection,
+        verticalDirection: verticalDirection,
+        textBaseline: textBaseline,
+        children: this,
+      );
+}
--- lib/wrap.dart
+++ lib/wrap.dart
@@ -1,5 +1,6 @@
 export 'src/align.dart';
 export 'src/color.dart';
 export 'src/decorate.dart';
+export 'src/list.dart';
 export 'src/media.dart';
 export 'src/text.dart';
