847ed43c3d670723dba4c2d08e86ae2f4849ba1c
James Aprosail <aprosail@outlook.com>
2024-05-14 14:11:28 +0800

    example with color

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -9,6 +9,8 @@ class App extends StatelessWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) =>
-      const Center(child: Text('app root')).ensureTextEnvironment(context);
+  Widget build(BuildContext context) => const Center(child: Text('app root'))
+      .wrapForeground(context, const Color(0xffdedcda))
+      .wrapBackground(const Color(0xff804044))
+      .ensureTextEnvironment(context);
 }
