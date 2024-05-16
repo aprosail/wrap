a42be978c9e04dc38b1b7327a16e6c3fed47c95f
James Aprosail <aprosail@outlook.com>
2024-05-17 00:23:18 +0800

    fix call set state when build bug

--- lib/src/size.dart
+++ lib/src/size.dart
@@ -3,21 +3,25 @@ import 'package:flutter/widgets.dart';
 extension WrapSize on Widget {
   /// Listen the size change of this widget and handle [listener] as hook.
   ///
-  /// This wrapper should be placed close to root of the widget tree,
-  /// the size is based on the context of this widget,
-  /// so that the inner size change listener should be close to
-  /// where the context is build, or there might be potential conflicts.
-  Widget listenSizeChange(
-    BuildContext context,
-    void Function(Size size) listener,
-  ) {
+  /// 1. The context is gotten from the [state] using [State.context].
+  /// 2. Don't call [State.setState] inside the [listener].
+  ///    [State.setState] will be called automatically post frame.
+  ///    Because calling it inside the [listener] will cause conflict
+  ///    for it will call [State.setState] when building.
+  ///
+  /// 3. This wrapper should be placed close to root of the widget tree,
+  ///    the size is based on the context of this widget,
+  ///    so that the inner size change listener should be close to
+  ///    where the context is build, or there might be potential conflicts.
+  Widget listenSizeChange(State state, void Function(Size size) listener) {
     return NotificationListener(
       onNotification: (notification) {
         if (notification is SizeChangedLayoutNotification) {
+          final context = state.context;
+          listener(context.size ?? MediaQuery.of(context).size);
           WidgetsBinding.instance.addPostFrameCallback(
-            (timestamp) => listener(
-              context.size ?? MediaQuery.of(context).size,
-            ),
+            // ignore: invalid_use_of_protected_member
+            (timestamp) => state.setState(() {}),
           );
           return true;
         }
