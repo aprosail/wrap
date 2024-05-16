01028be4f897441fbb2f17fc82c1221ac6e72e28
James Aprosail <aprosail@outlook.com>
2024-05-16 23:41:44 +0800

    doc warns about the size listener

--- lib/src/size.dart
+++ lib/src/size.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/widgets.dart';
 
 extension WrapSizeChange on Widget {
+  /// Listen the size change of this widget and handle [listener] as hook.
+  ///
+  /// This wrapper should be placed close to root of the widget tree,
+  /// the size is based on the context of this widget,
+  /// so that the inner size change listener should be close to
+  /// where the context is build, or there might be potential conflicts.
   Widget listenSizeChange(
     BuildContext context,
     void Function(Size size) listener,
