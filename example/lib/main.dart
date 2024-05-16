b1fc0b6dae5e15495bbba20de9c58df9e2e8e242
James Aprosail <aprosail@outlook.com>
2024-05-17 00:30:46 +0800

    example about size

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,15 +5,30 @@ void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
 
   @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  late var _height = MediaQuery.of(context).size.height;
+
+  @override
   Widget build(BuildContext context) => 'app root'
-      .textWidget
+      .text(align: TextAlign.center)
+      .modifyTextStyle(
+        context,
+        (raw) => raw.copyWith(
+          color: const Color(0xfffefdfa),
+          fontSize: _height / 6.18,
+          fontWeight: FontWeight.w900,
+        ),
+      )
+      .wrapPadding(const EdgeInsets.all(75))
       .wrapCenter
-      .wrap((context, child) => child.wrapFontSize(context, 50))
-      .wrapForeground(context, const Color(0xffdedcda))
       .wrapBackground(const Color(0xff804044))
+      .listenSizeChange(this, (size) => _height = size.height)
       .ensureTextEnvironment(context);
 }
