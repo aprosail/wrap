120863433422839f65e933c8a1d078bffecba68e
James Aprosail <aprosail@outlook.com>
2024-05-16 22:58:03 +0800

    example big font size

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -9,8 +9,10 @@ class App extends StatelessWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) => const Text('app root')
+  Widget build(BuildContext context) => 'app root'
+      .textWidget
       .wrapCenter
+      .wrap((context, child) => child.wrapFontSize(context, 50))
       .wrapForeground(context, const Color(0xffdedcda))
       .wrapBackground(const Color(0xff804044))
       .ensureTextEnvironment(context);
