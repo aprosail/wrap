9a38b371fe8059958814e485f393e94edbb64a2e
James Aprosail <aprosail@outlook.com>
2024-05-20 04:49:02 +0800

    update inherit handler

--- lib/src/handler.dart
+++ lib/src/handler.dart
@@ -30,9 +30,26 @@ class _InheritHandlerState<T> extends State<InheritHandler<T>> {
   }
 
   @override
-  Widget build(BuildContext context) => widget.child.wrapInherit(_data);
+  Widget build(BuildContext context) => widget.child
+      .wrapInherit(_data)
+      .wrapInheritUnchanged(InheritHandlerCaller<T>(
+        updater: (data) => this.data = data,
+      ));
+}
+
+class InheritHandlerCaller<T> {
+  const InheritHandlerCaller({required this.updater});
+
+  final void Function(T data) updater;
 }
 
 extension WrapHandler on Widget {
   Widget wrapHandler<T>(T data) => InheritHandler<T>(data: data, child: this);
+}
+
+extension UpdateHandlerData on BuildContext {
+  void maybeUpdate<T>(T data) => find<InheritHandlerCaller<T>>()?.updater(data);
+
+  void updateAndTrust<T>(T data) =>
+      findAndTrust<InheritHandlerCaller<T>>().updater(data);
 }
