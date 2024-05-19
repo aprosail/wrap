d016b824db9674d2a88d22e6d2bac126023f9e1d
James Aprosail <aprosail@outlook.com>
2024-05-20 04:27:39 +0800

    handler handle state

--- lib/src/handler.dart
+++ lib/src/handler.dart
@@ -17,8 +17,20 @@ class InheritHandler<T> extends StatefulWidget {
 }
 
 class _InheritHandlerState<T> extends State<InheritHandler<T>> {
+  late var _data = widget.data;
+  T get data => _data;
+  set data(T value) {
+    if (_data != value) setState(() => _data = value);
+  }
+
   @override
-  Widget build(BuildContext context) => widget.child.wrapInherit(widget.data);
+  void didUpdateWidget(covariant InheritHandler<T> oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    data = widget.data;
+  }
+
+  @override
+  Widget build(BuildContext context) => widget.child.wrapInherit(_data);
 }
 
 extension WrapHandler on Widget {
