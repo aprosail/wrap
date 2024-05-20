d3dc2dea294bf6bf3febeac32447c99621132972
James Aprosail <aprosail@outlook.com>
2024-05-20 15:37:31 +0800

    optimize builder apis

--- lib/src/context.dart
+++ lib/src/context.dart
@@ -1,16 +1,11 @@
 import 'package:flutter/widgets.dart';
 
 extension WrapContext on Widget {
+  /// Same as wrapping current widget with a [Builder], which keeps chain style.
   Widget wrap(Widget Function(BuildContext context, Widget child) builder) =>
-      ReContext((context) => builder(context, this));
+      Builder(builder: (context) => builder(context, this));
 }
 
-/// Refresh context, same as [Builder] but with conciser API.
-class ReContext extends StatelessWidget {
-  const ReContext(this.builder, {super.key});
-
-  final Widget Function(BuildContext context) builder;
-
-  @override
-  Widget build(BuildContext context) => builder(context);
-}
+/// An encapsulation of [Builder], which makes it conciser.
+Widget builder(Widget Function(BuildContext context) builder) =>
+    Builder(builder: builder);
